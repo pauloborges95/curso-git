cdaa0db9ffc4ee377db151a77f405227b73b52e0
Paulo <pauloborgesmartim@gmail.com>
2021-12-08 23:59:09 -0300

    Projato iniciado, mostrando ola mundo

---
+++ ProjetoTeste/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProjetoTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("olá mundo!");
+
+            Console.ReadLine();
+        }
+    }
+}
